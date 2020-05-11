cd604c0a581cfb6d72aa48eb2ffeb010303f5690
MohamedMedhat96 <mohamed.medhat-ali@student.guc.edu.eg>
2020-05-11 17:12:56 +0200

    Seperated the get latest song from update index method

--- src/app.js
+++ src/app.js
@@ -19,9 +19,9 @@ const headers = {
 app.get('/getSongByArtist', function (req, res) {
    var songQuery = req.query.query;
    var artistName = req.query.artistName;
-   if(songQuery==undefined || artistName==undefined)
-   return res.sendStatus(400).statusMessage("You need to send the song queyr and the artist name");
-   
+   if (songQuery == undefined || artistName == undefined)
+      return res.sendStatus(400).statusMessage("You need to send the song queyr and the artist name");
+
    var url = "https://bus.anghami.com/public/search";
    var outPut;
    getArtist(artistName, (localerr, localres) => {
@@ -50,33 +50,47 @@ app.get('/getSongByArtist', function (req, res) {
 
 })
 
+app.post('/updateIndex', function (req, res) {
+   indexService.getIndex((err, index) => {
+      if (err == undefined) {
+         indexService.updateIndex((JSON.parse(index) + 1).toString(), (updateErr, updateRes) => {
+            if (updateErr)
+               return res.status(500).send("An internal server error has occured, index was not updated: " + updateErr);
+            else
+               return res.status(200).send("Index updated succesfully");
+         }
+         )
+
+      }
+      else {
+         return res.status(500).send("An internal server error has occured, index was not updated");
+      }
+
+   })
+})
+
+
 app.get('/latestSong', function (req, res) {
    var url = "https://bus.anghami.com/public/playlist/data";
    var output;
-   
-    indexService.getIndex((err,index)=>{
-      var propertiesObject = { 'playlist_id': 175485967}
+
+   indexService.getIndex((err, index) => {
+      var propertiesObject = { 'playlist_id': 175485967 }
       console.log(index);
       request({ headers: headers, url: url, qs: propertiesObject }, function (err, response, body) {
          if (err) { console.log(err); return; }
          console.log("Get response: " + response.statusCode);
          outPut = JSON.parse(body);
-         if (outPut.data[index] != undefined){
-            indexService.updateIndex((JSON.parse(index) + 1).toString(), (updateErr,updateRes)=>{
-               if(updateErr==undefined)
-               console.log(updateErr);
-               else
-               console.log(updateRes);
-            })
+         if (outPut.data[index] != undefined) {
             res.send({ 'title': outPut.data[index].title, 'artist': outPut.data[index].artist, url: 'https://play.anghami.com/song/' + outPut.data[index].id });
-         }else
+         } else
             res.send("No song has been posted yet");
       });
-      
+
 
    });
-   
-   
+
+
 })
 
 app.listen(port, function () {
