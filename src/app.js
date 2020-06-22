4b36810a14ed3b6fb7e3c9811fa6c09731ef5237
MohamedMedhat96 <mohamed.medhat-ali@student.guc.edu.eg>
2020-06-22 11:19:36 +0200

    Added authentication and fixing issues with spotify and anghami playlist

--- src/app.js
+++ src/app.js
@@ -11,159 +11,149 @@ const app = express();
 
 app.use(bodyParser.json());
 
-var spotifyToken;
 
 spotifyService.spotifyLogin((err, res) => {
 
 })
 
-app.get('/getSongByArtistSpotify', function (req, res) {
+app.get('/getSongByArtistSpotify', function(req, res) {
 
-   var songQuery = req.query.query;
-   var artistName = req.query.artistName;
-   if (songQuery == undefined || artistName == undefined)
-      return res.status(400).send("You need to send the song query and the artist name");
-   spotifyService.spotifyGetSong(artistName, songQuery, (err, response) => {
-      if (err)
-         return res.status(500).send(err);
-      else
-         return res.send(response);
-   })
+    var songQuery = req.query.query;
+    var artistName = req.query.artistName;
+    if (songQuery == undefined || artistName == undefined)
+        return res.status(400).send("You need to send the song query and the artist name");
+    spotifyService.spotifyGetSong(artistName, songQuery, (err, response) => {
+        if (err)
+            return res.status(500).send(err);
+        else
+            return res.send(response);
+    })
 })
 
-app.post('/updateAPI', function (req, res) {
+app.post('/updateAPI', function(req, res) {
 
-   spotifyService.spotifyLogin((error, update) => {
-      if (error)
-         res.status(500).send(error)
-      else
-         res.status(200).send()
-   })
+    spotifyService.spotifyLogin((error, update) => {
+        if (error)
+            res.status(500).send(error)
+        else
+            res.status(200).send()
+    })
 })
 
 
-app.get('/getBuildStatus', function (req, res) {
-   firebaseService.getStatus((error, response) => {
-      if (error)
-         res.status(500).send(error)
-      else
-         res.status(200).send(response)
-   })
+app.get('/getBuildStatus', function(req, res) {
+    firebaseService.getStatus((error, response) => {
+        if (error)
+            res.status(500).send(error)
+        else
+            res.status(200).send(response)
+    })
 })
 
-app.post('/updateBuildStatus', function (req, res) {
-   var status = req.body;
-   firebaseService.updateStatus(status, (err, response) => {
-      if (err)
-         return res.status(500).send(err);
-      else {
-         //console.log(response)
-         return res.send(response);
-      }
-   })
+app.post('/updateBuildStatus', function(req, res) {
+    var status = req.body;
+    firebaseService.updateStatus(status, (err, response) => {
+        if (err)
+            return res.status(500).send(err);
+        else {
+            //console.log(response)
+            return res.send(response);
+        }
+    })
 })
 
-app.get('/getSongByArtist', function (req, res) {
-   var songQuery = req.query.query;
-   var artistName = req.query.artistName;
-   if (songQuery == undefined || artistName == undefined)
-      return res.status(400).send("You need to send the song query and the artist name");
+app.get('/getSongByArtist', function(req, res) {
+    var songQuery = req.query.query;
+    var artistName = req.query.artistName;
+    if (songQuery == undefined || artistName == undefined)
+        return res.status(400).send("You need to send the song query and the artist name");
 
-   var url = "https://bus.anghami.com/public/search";
-   var outPut;
-   anghamiService.getArtist(artistName, (localerr, localres) => {
+    var url = "https://bus.anghami.com/public/search";
+    var outPut;
+    anghamiService.getArtist(artistName, (localerr, localres) => {
 
-      if (localerr == undefined) {
-         anghamiService.getSongByArtist(localres.id, songQuery, (songErr, songRes) => {
-            if (songErr) {
-               anghamiService.getSong(artistName, songQuery, (searchErr, searchRes) => {
-                  if (searchErr)
-                     return res.status(500).send(searchErr);
-                  else
-                     return res.send(searchRes);
-               })
-            }
-            else
-               return res.send(songRes);
-         })
-      }
-      else
-         anghamiService.getSong(artistName, songQuery, (searchErr, searchRes) => {
-            if (searchErr)
-               return res.status(500).send(searchErr);
-            else
-               return res.send(searchRes);
-         })
+        if (localerr == undefined) {
+            anghamiService.getSongByArtist(localres.id, songQuery, (songErr, songRes) => {
+                if (songErr) {
+                    anghamiService.getSong(artistName, songQuery, (searchErr, searchRes) => {
+                        if (searchErr)
+                            return res.status(500).send(searchErr);
+                        else
+                            return res.send(searchRes);
+                    })
+                } else
+                    return res.send(songRes);
+            })
+        } else
+            anghamiService.getSong(artistName, songQuery, (searchErr, searchRes) => {
+                if (searchErr)
+                    return res.status(500).send(searchErr);
+                else
+                    return res.send(searchRes);
+            })
 
 
-   })
+    })
 
 
 
 
 })
 
-app.post('/setIndex', function (req, res) {
-   var indexValue = req.query.index;
+app.post('/setIndex', function(req, res) {
+    var indexValue = req.query.index;
 
-   firebaseService.updateIndex(indexValue, (err, index) => {
-      if (err)
-         return res.status(500).send("An internal server error has occured, index was not updated: " + updateErr);
-      else
-         return res.status(200).send("Index updated succesfully, Index:" + index);
-   })
+    firebaseService.updateIndex(indexValue, (err, index) => {
+        if (err)
+            return res.status(500).send("An internal server error has occured, index was not updated: " + updateErr);
+        else
+            return res.status(200).send("Index updated succesfully, Index:" + index);
+    })
 })
 
-app.post('/updateIndex', function (req, res) {
-   firebaseService.getIndex((err, index) => {
-      if (err == undefined) {
-         firebaseService.updateIndex((JSON.parse(index) + 1).toString(), (updateErr, updateRes) => {
-            if (updateErr)
-               return res.status(500).send("An internal server error has occured, index was not updated: " + updateErr);
-            else
-               return res.status(200).send("Index updated succesfully");
-         }
-         )
+app.post('/updateIndex', function(req, res) {
+    firebaseService.getIndex((err, index) => {
+        if (err == undefined) {
+            firebaseService.updateIndex((JSON.parse(index) + 1).toString(), (updateErr, updateRes) => {
+                if (updateErr)
+                    return res.status(500).send("An internal server error has occured, index was not updated: " + updateErr);
+                else
+                    return res.status(200).send("Index updated succesfully");
+            })
 
-      }
-      else {
-         return res.status(500).send("An internal server error has occured, index was not updated");
-      }
+        } else {
+            return res.status(500).send("An internal server error has occured, index was not updated");
+        }
 
-   })
+    })
 })
 
 
 
-app.get('/latestSong', function (req, res) {
-   anghamiService.getLatestSong((err, body) => {
-      if (err)
-         res.status(500).send(err);
-      else
-         res.send(body);
-   })
+app.get('/latestSong', function(req, res) {
+    anghamiService.getLatestSong((err, body) => {
+        if (err)
+            res.status(500).send(err);
+        else
+            res.send(body);
+    })
 
 })
 
-app.get('/updateAccumulatorPlaylist', function (req, res) {
-      anghamiService.updateAccumulatorPlaylist( req.songID, (err, body) => {
-         if (err)
-         res.status(500).send(err);
-      else
-         res.send(body);
-      }
-      )
-      
-      spotifyService.spotifyUpdateAccumulatorPlaylist(req.songURI, (err, body) => {
-         if (err)
-         res.status(500).send(err);
-      else
-         res.send(body);
-      }
-      )
+app.get('/updateAccumulatorPlaylist', function(req, res) {
+    console.log(req.body)
+    anghamiService.updateAccumulatorPlaylist(req.body.songID, (err, body) => {
+
+        spotifyService.spotifyUpdateAccumulatorPlaylist(req.body.songURI, (err, body) => {
+            console.log("test")
+            res.send()
+        })
+    })
+
+
 })
 
 
-app.listen(port, function () {
-   console.log("A7zanBot is listening on port:%s", port);
+app.listen(port, function() {
+    console.log("A7zanBot is listening on port:%s", port);
 })
--- utils/spotifyService.js
+++ utils/spotifyService.js
@@ -1,84 +1,92 @@
-
-const request = require('request');
-const spotifyClientId = process.env.CLIENTID;
-const spotifyClientSecret = process.env.CLIENTSECRET;
-const accumulatorPlaylistID = 186329603;
+const request = require('request')
+const spotifyClientId = process.env.CLIENTID
+const spotifyClientSecret = process.env.CLIENTSECRET
+const accumulatorPlaylistID = "1L92urVus8OP0CGGXEQa2l"
+const refreshToken = process.env.REFRESHTOKEN
 var spotifyToken;
 const spotifyLogin = (callback) => {
-  const spotifyOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-      'Authorization': 'Basic ' + (Buffer.from(spotifyClientId + ':' + spotifyClientSecret).toString('base64'))
-    },
-    form: {
-      grant_type: 'client_credentials'
-    },
-    json: true
-  };
-  request.post(spotifyOptions, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      spotifyToken = body.access_token;
-      callback(undefined, spotifyToken);
-    } else {
-      callback(error, undefined);
-    }
-  })
+    const spotifyOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+            'Authorization': 'Basic ' + (Buffer.from(spotifyClientId + ':' + spotifyClientSecret).toString('base64')),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        form: {
+            grant_type: 'refresh_token',
+            refresh_token: refreshToken
+        },
+        json: true
+    };
+    request.post(spotifyOptions, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            spotifyToken = body.access_token;
+            console.log(spotifyToken)
+            callback(undefined, spotifyToken);
+        } else {
+            console.log(body)
+            callback(error, undefined);
+        }
+    })
 }
 
 
 
 const spotifyGetSong = (artistName, songQuery, callback) => {
-  var spotifyOptions = {
-    url: 'https://api.spotify.com/v1/search',
-    headers: {
-      'Authorization': 'Bearer ' + spotifyToken
-    },
-    json: true
-  };
-  spotifyOptions.url = spotifyOptions.url + '?q=track:' + songQuery.replace(/\s/g, "%20") + '%20artist:' + artistName.replace(/\s/g, "%20") + '&type=track'
-  request.get(spotifyOptions, function (error, response, body) {
-    console.log(spotifyOptions.url)
-    if (!error) {
-      if (body != undefined && body.tracks!=undefined && body.tracks.items[0]!= undefined) {
-        var response = { songUrl: body.tracks.items[0].external_urls.spotify, artist: body.tracks.items[0].artists[0].name, title : body.tracks.items[0].name }
-        console.log(response)
-        callback(undefined, response)
-      } else {
-        console.log(body)
-        callback({error:"Song not found on spotify"}, undefined);
-      }
-    } else {
-      console.log(error)
-      callback(error, undefined);
-    }
-  })
+    var spotifyOptions = {
+        url: 'https://api.spotify.com/v1/search',
+        headers: {
+            'Authorization': 'Bearer ' + spotifyToken
+        },
+        json: true
+    };
+    spotifyOptions.url = spotifyOptions.url + '?q=track:' + songQuery.replace(/\s/g, "%20") + '%20artist:' + artistName.replace(/\s/g, "%20") + '&type=track'
+    request.get(spotifyOptions, function(error, response, body) {
+        console.log(spotifyOptions.url)
+        if (!error) {
+            if (body != undefined && body.tracks != undefined && body.tracks.items[0] != undefined) {
+                var response = { songUrl: body.tracks.items[0].external_urls.spotify, artist: body.tracks.items[0].artists[0].name, title: body.tracks.items[0].name }
+                console.log(response)
+                callback(undefined, response)
+            } else {
+                console.log(body)
+                callback({ error: "Song not found on spotify" }, undefined);
+            }
+        } else {
+            console.log(error)
+            callback(error, undefined);
+        }
+    })
 }
 
 const spotifyUpdateAccumulatorPlaylist = (songURI, callback) => {
-  var spotifyOptions = {
-    url: 'https://api.spotify.com/v1/playlists/' + accumulatorPlaylistID + '/tracks',
-    headers: {
-      'Authorization': 'Bearer ' + spotifyToken,
-      'Content-Type': 'application/json'
-    },
-    json: true
-  };
-  spotifyOptions.url = spotifyOptions.url + '?uris=' + songURI;
-  request.post(spotifyOptions, function (error, response, body) {
-    console.log(spotifyOptions.url)
-    if (error) {
-      return callback(err, undefined);
-   }
-   if(response.statusCode!=200){
-      console.log("Error:" +response.statusCode+ response.statusMessage);
-   }
-   else {
-      console.log("song successfully added to spotify playlist!");
-   }
-  })
+    console.log(songURI)
+    var spotifyOptions = {
+        url: 'https://api.spotify.com/v1/playlists/' + accumulatorPlaylistID + '/tracks',
+        headers: {
+            'Authorization': 'Bearer ' + spotifyToken,
+            'Content-Type': 'application/json'
+        },
+        json: true
+    };
+    spotifyOptions.url = spotifyOptions.url + '?uris=' + songURI;
+    request.post(spotifyOptions, function(error, response, body) {
+        console.log(spotifyOptions.url)
+        if (error) {
+            return callback(err, undefined);
+        }
+        if (response.statusCode != 201) {
+            console.log(response)
+            console.log("Error:" + response.statusCode + response.statusMessage);
+        } else {
+            console.log("song successfully added to spotify playlist!");
+            callback(undefined, "song added")
+        }
+    })
 
 }
 
 module.exports = {
-  spotifyLogin, spotifyGetSong, spotifyUpdateAccumulatorPlaylist
+    spotifyLogin,
+    spotifyGetSong,
+    spotifyUpdateAccumulatorPlaylist
 }
